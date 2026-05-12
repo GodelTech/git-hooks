28fb6a42bf89ce12b6cb12efcaa90f250af98aa5
Vladimir Rodchenko <vladimir@rodchenko.by>
2026-05-12 18:50:23 +0200

    editorconfig

--- src/GitHooks.Workflow.Domain/Model/SourceSpan.cs
+++ src/GitHooks.Workflow.Domain/Model/SourceSpan.cs
@@ -3,15 +3,14 @@ namespace GitHooks.Workflow.Domain.Model;
 public readonly record struct SourceSpan(
     SourceRef Source,
     SourceLocation Start,
-    SourceLocation End
-)
+    SourceLocation End)
 {
     public static SourceSpan Unknown(SourceRef source)
     {
         return new SourceSpan(
-                source,
-                new SourceLocation(0, 0),
-                new SourceLocation(0, 0)
-            );
+            source,
+            new SourceLocation(0, 0),
+            new SourceLocation(0, 0)
+        );
     }
 }
